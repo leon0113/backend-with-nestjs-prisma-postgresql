442215c9407bbc1075147939c0671cffbfc154bb
Leon <tahjib.leon399@gmail.com>
2024-09-02 12:14:55 +0600

    employee service logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
